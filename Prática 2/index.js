28b85ab808b7d15d5a4ef8b32d7e621daf33e028
vitorDaynno <vitordaynno@gmail.com>
2020-08-02 18:04:09 -0300

    Finalização da Prática 2

--- Prática 2/index.js
+++ Prática 2/index.js
@@ -1,9 +1,26 @@
 import { promises as fs, stat } from 'fs';
 
 const start = async () => {
+  console.log('Atividade 1:');
   await generateFiles();
+
+  console.log('\nAtividade 3:');
   await statesWithMoreCities();
+
+  console.log('\nAtividade 4:');
   await statesWithLessCities();
+
+  console.log('\nAtividade 5:');
+  await biggestCityNames();
+
+  console.log('\nAtividade 6:');
+  await minorCityNames();
+
+  console.log('\nAtividade 7:');
+  await biggestNameCityInStates();
+
+  console.log('\nAtividade 8:');
+  await smallestNameCityInStates();
 };
 
 const generateFiles = async () => {
@@ -84,6 +101,91 @@ const statesWithLessCities = async () => {
     return `${stateInfo.initials} - ${stateInfo.quantityCities}`;
   });
   console.log(statesCitiesCount);
+};
+
+const getBiggestCityNames = async () => {
+  const biggestCityNames = [];
+  const states = await getStates();
+  const statesInfo = await getCitiesInfo(states);
+
+  for (const state of statesInfo) {
+    const cities = state.cities;
+    cities.sort((a, b) => b.sizeName - a.sizeName);
+    biggestCityNames.push({ city: cities[0].name, state: state.initials });
+  }
+
+  return biggestCityNames;
+};
+
+const getCitiesInfo = async (states) => {
+  const citiesInfo = [];
+
+  for (const state of states) {
+    const initials = state.Sigla;
+
+    const filename = initials.toUpperCase();
+    const path = `states/${filename}.json`;
+    let cities = await getInfoByFile(path);
+    cities = cities.map((city) => {
+      const { ID, Nome } = city;
+      const id = ID;
+      const name = Nome;
+      const sizeName = name.length;
+      return { id, name, sizeName };
+    });
+
+    citiesInfo.push({ initials, cities });
+  }
+
+  return citiesInfo;
+};
+
+const getMinorCityNames = async () => {
+  const minorCityNames = [];
+  const states = await getStates();
+  const statesInfo = await getCitiesInfo(states);
+
+  for (const state of statesInfo) {
+    const cities = state.cities;
+    cities.sort((a, b) => a.sizeName - b.sizeName);
+    minorCityNames.push({ city: cities[0].name, state: state.initials });
+  }
+
+  return minorCityNames;
+};
+
+const biggestCityNames = async () => {
+  const biggestCityNames = await getBiggestCityNames();
+  const result = biggestCityNames.map((biggestCity) => {
+    const { state, city } = biggestCity;
+    return `${city} - ${state}`;
+  });
+  console.log(result);
+};
+
+const minorCityNames = async () => {
+  const minorCityNames = await getMinorCityNames();
+  const result = minorCityNames.map((minorCity) => {
+    const { state, city } = minorCity;
+    return `${city} - ${state}`;
+  });
+  console.log(result);
+};
+
+const biggestNameCityInStates = async () => {
+  const biggestCityNames = await getBiggestCityNames();
+  biggestCityNames.sort((a, b) => {
+    return b.city.length - a.city.length;
+  });
+  console.log(biggestCityNames[0].city);
+};
+
+const smallestNameCityInStates = async () => {
+  const smallestNameCityInStates = await getMinorCityNames();
+  smallestNameCityInStates.sort((a, b) => {
+    return b.city.length - a.city.length;
+  });
+  console.log(smallestNameCityInStates[0].city);
 };
 
 start();
