0f834b4224d5df7830713ccbce07f388988cc9b7
vitorDaynno <vitordaynno@gmail.com>
2020-08-02 02:39:53 -0300

    Método de estados com mais e menos cidades implementados

--- Prática 2/index.js
+++ Prática 2/index.js
@@ -1,9 +1,9 @@
-import { promises as fs } from 'fs';
+import { promises as fs, stat } from 'fs';
 
 const start = async () => {
-  generateFiles();
-  const quantityCity = await countCities('MG');
-  console.log(quantityCity);
+  await generateFiles();
+  await statesWithMoreCities();
+  await statesWithLessCities();
 };
 
 const generateFiles = async () => {
@@ -30,14 +30,17 @@ const getInfoByFile = async (path) => {
   return JSON.parse(contentFile);
 };
 
-const generateStateFile = (state, cities) => {
+const generateStateFile = async (state, cities) => {
   const citiesInState = [];
   cities.forEach((city) => {
     if (state.ID === city.Estado) {
       citiesInState.push(city);
     }
   });
-  fs.writeFile(`./states/${state.Sigla}.json`, JSON.stringify(citiesInState));
+  await fs.writeFile(
+    `./states/${state.Sigla}.json`,
+    JSON.stringify(citiesInState)
+  );
 };
 
 const countCities = async (state) => {
@@ -45,6 +48,42 @@ const countCities = async (state) => {
   const path = `states/${filename}.json`;
   const cities = await getInfoByFile(path);
   return cities.length;
+};
+
+const statesWithMoreCities = async () => {
+  const states = await getStates();
+  const statesInfo = await getStatesInfo(states);
+  const statesWithMoreCities = statesInfo.splice(-5).reverse();
+  const statesCitiesCount = statesWithMoreCities.map((stateInfo) => {
+    return `${stateInfo.initials} - ${stateInfo.quantityCities}`;
+  });
+  console.log(statesCitiesCount);
+};
+
+const getStatesInfo = async (states) => {
+  const statesInfo = [];
+
+  for (const state of states) {
+    const initials = state.Sigla;
+    const quantityCities = await countCities(initials);
+
+    const stateInfo = { initials, quantityCities };
+    statesInfo.push(stateInfo);
+  }
+
+  statesInfo.sort((a, b) => a.quantityCities - b.quantityCities);
+
+  return statesInfo;
+};
+
+const statesWithLessCities = async () => {
+  const states = await getStates();
+  const statesInfo = await getStatesInfo(states);
+  const statesWithLessCities = statesInfo.splice(0, 5);
+  const statesCitiesCount = statesWithLessCities.map((stateInfo) => {
+    return `${stateInfo.initials} - ${stateInfo.quantityCities}`;
+  });
+  console.log(statesCitiesCount);
 };
 
 start();
